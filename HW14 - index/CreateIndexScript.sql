714e0daee1b5d798ac96a1cfcf3f6947005a1580
artem <test@mail.com>
2020-06-07 05:47:36 -0700

    Home work 14 - index

---
+++ HW14 - index/CreateIndexScript.sql
@@ -0,0 +1,163 @@
+﻿use Webshop7
+
+CREATE FULLTEXT CATALOG WebshopFullTextCatalog 
+WITH ACCENT_SENSITIVITY = ON
+AS DEFAULT
+AUTHORIZATION [dbo]
+
+GO
+
+-- Создаем Full-Text Index
+CREATE FULLTEXT INDEX ON Product( [Name] LANGUAGE English )
+KEY INDEX PK_Product ON ( WebshopFullTextCatalog )
+WITH
+(
+  CHANGE_TRACKING = AUTO,
+  STOPLIST = SYSTEM
+);
+GO
+
+-- Включаем Full-Text Index
+ALTER FULLTEXT INDEX ON Product ENABLE;
+
+GO
+
+CREATE FULLTEXT INDEX ON ProductName( [Name] LANGUAGE English )
+KEY INDEX PK_ProductName ON ( WebshopFullTextCatalog )
+WITH
+(
+  CHANGE_TRACKING = AUTO,
+  STOPLIST = SYSTEM
+);
+GO
+
+-- Включаем Full-Text Index
+ALTER FULLTEXT INDEX ON ProductName ENABLE;
+
+GO
+
+--Подсистема адреса:
+
+CREATE INDEX IX_Country_Name ON Country( [Name] )
+
+GO
+
+CREATE INDEX IX_Region_Name ON Region( [Name] )
+
+GO
+
+CREATE INDEX IX_Region_CountryId ON Region( CountryId )
+
+GO
+
+CREATE INDEX IX_City_Name ON City( [Name] )
+
+GO
+
+CREATE INDEX IX_City_CountryId ON City( CountryId )
+
+GO
+
+CREATE INDEX IX_City_RegionId ON City( RegionId )
+
+GO
+
+CREATE INDEX IX_PostalCode_CityId ON PostalCode( CityId )
+
+GO
+
+CREATE INDEX IX_PostalCode_PostalCode ON PostalCode( PostalCode )
+
+GO
+
+--Подсистема продукта:
+
+CREATE INDEX IX_Product_BrandId ON Product( BrandId )
+
+GO
+
+CREATE INDEX IX_Product_CategoryId ON Product( CategoryId )
+
+GO
+
+CREATE INDEX IX_Product_Sku ON Product( Sku )
+
+GO
+
+CREATE UNIQUE INDEX UX_ProductName_ProductId_LanguageId ON ProductName( ProductId, LanguageId )
+
+GO
+
+CREATE INDEX IX_ProductDocument_ProductId ON ProductDocument( ProductId )
+
+GO
+
+CREATE UNIQUE INDEX UX_Document_DocumentHash ON Document( DocumentHash )
+
+GO
+
+CREATE INDEX IX_Document_Source ON Document( Source )
+
+GO
+
+CREATE INDEX IX_Document_Code ON Document( Code )
+
+GO
+
+CREATE UNIQUE INDEX UX_Brand_Name ON Brand( [Name] )
+
+GO
+
+CREATE INDEX IX_Category_ParentCategoryId ON Category( ParentCategoryId )
+
+GO
+
+CREATE INDEX IX_Category_Name ON Category( [Name] )
+
+GO
+
+CREATE INDEX IX_CategoryName_Name ON CategoryName( [Name] )
+
+GO
+
+CREATE INDEX UX_CategoryName_CategoryId_LanguageId ON CategoryName( CategoryId, LanguageId )
+
+GO
+
+CREATE INDEX IX_ProductRating_UserId ON ProductRating( UserId )
+
+GO
+
+CREATE INDEX IX_User_Email ON [User]( Email ) INCLUDE ( PasswordHash )
+
+GO
+
+CREATE INDEX UX_UserWishProduct_UserWishId_ProductId ON UserWishProduct( UserWishId, ProductId )
+
+GO
+
+CREATE INDEX IX_UserWish_UserId ON UserWish( UserId )
+
+GO
+
+--Подсистема сейлс ордер и корзины:
+
+CREATE INDEX IX_SalesOrder_UserId ON SalesOrder( UserId )
+
+GO
+
+CREATE INDEX IX_SalesOrderLine_SalesOrderId ON SalesOrderLine( SalesOrderId )
+
+GO
+
+CREATE INDEX IX_Address_UserId ON Address( UserId )
+
+GO
+
+CREATE INDEX IX_ShoppingCart_UserId ON ShoppingCart( UserId )
+
+GO
+
+CREATE INDEX IX_ShoppingCartLine_ShoppingCartId ON ShoppingCartLine( ShoppingCartId )
+
+
---
+++ HW14 - index/IndexList.sql
@@ -0,0 +1,89 @@
+﻿
+-- Какие индексы вам нужны
+-- Думаем какие запросы у вас будут в базе и добавляем для них индексы. Проверяем, что они используются в запросе.
+
+
+Краткое описание
+
+В проекте используется обычные индексы, уникальные индексы и полнотекстовый поиск.
+
+Например:
+
+Country.Name, Brand.Name  - уникальные индексы, так как повторений в имнах стран и брендов быть не может, но при этом эти поля учавствуют в поиске.
+
+ProductName.ProductId + ProductName.LanguageId - уникальный индекс по двум полям, подразумевается, что будет запрос с поиском имени продукта для конкретного языка, например:
+
+SELECT ProductId, Name 
+FROM ProductName
+WHERE ( ProductId = 112233 ) AND ( LangueageId = 1 )
+
+
+User.Email - индекс с включением паролья, для ускорения поиска пользователя:
+
+SELECT 1
+FROM User
+WHWRE ( Email = 'xxxx@mail.com' ) AND ( PasswordHash = 'some_password_hash' )
+
+Пример полнотекстового поиска:
+
+SELECT ProductId, Name
+FROM Product
+WHERE FREETEXT ( Name, N'iphone or ipod' )
+
+
+Полный список индексов:
+
+Подсистема адреса:
+
+- Country.Name - уникальный индекс, для быстрого поиска страны
+
+- Region.Name - простой индекс
+- Region.CountryId - простой индекс
+
+- City.Name - простой индекс
+- City.CountryId - простой индекс
+- City.RegionId - простой индекс
+
+- PostalCode.CityId - простой индекс
+- PostalCode.PostalCode - простой индекс
+
+
+Подсистема продукта:
+
+- Product.Name - полнотекстовый поиск
+- Product.BrandId - простой индекс
+- Product.CategoryId - простой индекс
+- Product.Sku - простой индекс
+- ProductName.Name - полнотекстовый поиск
+- ProductName.ProductId + ProductName.LanguageId - уникальный индекс
+
+- ProductDocument.ProductId - простой индекс
+
+- Document.DocumentHash - уникальный индекс
+- Document.Source - прстой индекс
+- Document.Code - простой индекс
+
+- Brand.Name - уникальный индекс
+
+- Category.ParentCategoryId - простой индекс
+- Category.Name - простой индекс
+- CategoryName.Name - простой индекс
+- CategoryName.CategoryId + CategoryName.LanguageId - уникальный индекс
+
+- ProductRating.UserId
+
+- User.Email - простой индекс + включить в этот индекс поле пассворда, для более быстрого поиска
+
+- UserWishProduct.UserWishId + UserWishProduct.ProductId - уникальный констреинт
+- UserWish.UserId - простой индекс
+
+
+Подсистема сейлс ордер и корзины:
+
+- SalesOrder.UserId - простой индекс
+- SalesOrderLine.SalesOrderId - простой индекс
+
+- Address.UserId - простой индекс
+
+- ShoppingCart.UserId - простой индекс
+- ShoppingCartLine.ShoppingCartId - простой индекс
